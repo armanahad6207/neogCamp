ea8128ecd484429092e1d99b9861be5e6a7e5b51
armanahad6207 <armanahad7549@gmail.com>
2024-11-12 18:08:37 +0530

    A2.2_HW_2

---
+++ levelZero/A2.2_HW_2.js
@@ -0,0 +1,93 @@
+console.log("A2.2_HW_2.js");
+
+console.log("---- ---- ----");
+console.log("Exercise 1");
+console.log("---- ---- ----");
+
+console.log("1.1");
+
+let rajeshRating = 8.2;
+let amitRating = 7.9;
+let isRajeshHigherRated = rajeshRating > amitRating;
+console.log("Is Rajesh higher rated than Amit?", isRajeshHigherRated);
+
+console.log("---- ---- ----");
+
+console.log("1.2");
+
+let rajeshCollection = 250;
+let amitCollection = 350;
+let isRajeshHigherCollection = rajeshCollection > amitCollection;
+console.log(
+  "Does Rajesh have a higher superhit movie collection than Amit?",
+  isRajeshHigherCollection
+);
+
+console.log("---- ---- ----");
+
+console.log("1.3");
+
+let rajeshIncome = 50;
+let amitIncome = 80;
+let isRajeshLessIncome = rajeshIncome < amitIncome;
+console.log("Does Rajesh have a lower income than Amit?", isRajeshLessIncome);
+
+console.log("---- ---- ----");
+
+console.log("1.4");
+
+let rajeshAds = 10;
+let amitAds = 8;
+let isRajeshMoreAds = rajeshAds > amitAds;
+console.log("Has rajesh done more ads than Amit?", isRajeshMoreAds);
+
+console.log("---- ---- ----");
+
+console.log("1.2");
+
+let rajeshFans = 2000000;
+let amitFans = 3500000;
+let isRajeshMoreFans = rajeshFans > amitFans;
+console.log("Does Rajesh havemore fans than Amit?", isRajeshMoreFans);
+
+console.log("---- ---- ----");
+console.log("Exercise 2");
+console.log("---- ---- ----");
+
+console.log("2.1");
+let tShirtPrice = 799;
+let jeansrice = 599;
+
+let budget = 1500
+let totalPrice = tShirtPrice + jeansrice
+let areBothUnderBudget = budget >= totalPrice
+
+console.log("Can I buy both a T-shirt and jeans within my budget?", areBothUnderBudget)
+
+console.log("---- ---- ----")
+
+console.log("2.2")
+let mathScore = 85
+let englishScore = 92
+let physicScore = 78
+
+let minmumAverage = 85
+
+let averageScore = (mathScore + englishScore + physicScore) / 3
+ 
+let isQualified = averageScore >= minmumAverage
+
+console.log("Am I qualified for the olampiad?",isQualified)
+
+console.log("---- ---- ----")
+
+console.log("2.3")
+
+let productQunatity = 60
+
+let isDivisibleBy5 = productQunatity % 5 === 0
+let isDivisibleBy10 = productQunatity % 10 === 0
+
+console.log("can the product quantity be sold in the batches of 5?", isDivisibleBy5)
+console.log("can the product quantity be sold in the batches of 10?", isDivisibleBy10)
+
