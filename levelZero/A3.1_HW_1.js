4080ebda3837c8dc3c906a7d97c0dd4855b45c80
armanahad6207 <armanahad7549@gmail.com>
2024-11-16 16:42:56 +0530

    A3.1_HW_1

---
+++ levelZero/A3.1_HW_1.js
@@ -0,0 +1,73 @@
+console.log("A3.1_HW_1");
+console.log("---- ---- ----");
+
+console.log("1.1");
+let age = 18;
+let isAdult = true;
+if (isAdult) {
+  console.log("Adult");
+  console.log("Age:", age);
+}
+
+console.log("---- ---- ----");
+
+console.log("1.2");
+let customAge = 22;
+let baseTicketPrice = 200;
+
+if (customAge <= 18) {
+  baseTicketPrice = baseTicketPrice - baseTicketPrice * 0.2;
+  console.log("Ticket Price:", baseTicketPrice);
+} else {
+  console.log("Ticket Price:", baseTicketPrice);
+}
+
+console.log("---- ---- ----");
+
+console.log("1.3");
+let num1 = 25;
+let num2 = 50;
+let num3 = 20;
+
+if (num3 < num1 && num3 < num2) {
+  console.log("The third number " + num3 + "," + " is the smallest ");
+} else {
+  console.log("The third number " + num3 + "," + " is not the smallest ");
+}
+
+console.log("---- ---- ----");
+
+console.log("1.4");
+
+let book1 = "Harry Potter and the Sorcerer's Stone";
+let book2 = "The Great Gatsby";
+let book3 = "To kill a Mockingbird";
+let book4 = "The Lord of the Rings";
+
+let price1 = 400;
+let price2 = 300;
+let price3 = 250;
+let price4 = 500;
+
+let totalCartPrice = price1 + price2 + price3 + price4;
+let deliveryChargeStatus = "";
+
+if (totalCartPrice < 799) {
+  let deliveryCharge = 50;
+  totalCartPrice = totalCartPrice + deliveryCharge;
+  deliveryChargeStatus = "Optional Delivery Charge:" + deliveryCharge + ".";
+} else {
+  deliveryChargeStatus = "No Delivery Charge";
+}
+
+console.log("*** Bookstore Shopping Cart Summary ***");
+console.log("Book1:", book1 + " - " + "₹" + price1);
+console.log("Book2:", book2 + " - " + "₹" + price2);
+console.log("Book3:", book3 + " - " + "₹" + price3);
+console.log("Book4:", book4 + " - " + "₹" + price4);
+
+console.log("--------------------------------------------");
+
+console.log("Total Cart Price:" + " ₹" + totalCartPrice);
+
+console.log("Delivery Charges:", deliveryChargeStatus);
